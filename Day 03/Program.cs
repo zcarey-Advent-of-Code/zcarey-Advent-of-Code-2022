78f31e2433f9bab0f4a2a77e4906d31ad59a8457
Zachary Carey <zach.d.carey@gmail.com>
2022-12-08 17:02:02 -0500

    Day 03 pt 2

--- Day 03/Program.cs
+++ Day 03/Program.cs
@@ -29,7 +29,36 @@ namespace Day_03 {
         }
 
         protected override object SolvePart2(IEnumerable<string[]> input) {
-            return null;
+            int sum = 0;
+
+            // For each group of three,
+            Dictionary<char, int> items = CreateEmptyDictionary();
+            int rucksackCount = 0;
+            foreach(string[] rucksack in input) {
+                rucksackCount++;
+
+                // Count occurances of all items in the rucksack
+                foreach(char item in rucksack[0].Concat(rucksack[1]).Distinct()) {
+                    items[item]++;
+                }
+
+                if (rucksackCount == 3) {
+                    // Found a group of 3 elves, find the item with only a count of 3
+                    foreach(KeyValuePair<char, int> pair in items) {
+                        if (pair.Value == 3) {
+                            // Item found!
+                            sum += Priority(pair.Key);
+                            break;
+                        }
+                    }
+
+                    // Reset for next group of 3
+                    rucksackCount = 0;
+                    items = CreateEmptyDictionary();
+                }
+            }
+
+            return sum;
         }
 
         public static int Priority(char c) {
@@ -38,6 +67,17 @@ namespace Day_03 {
             } else {
                 return (c - 'A') + 27;
             }
+        }
+
+        private static Dictionary<char, int> CreateEmptyDictionary() {
+            Dictionary<char, int> items = new();
+            for(char c = 'a'; c <= 'z'; c++) {
+                items[c] = 0;
+            }
+            for(char c = 'A'; c <= 'Z'; c++) {
+                items[c] = 0;
+            }
+            return items;
         }
 
     }
