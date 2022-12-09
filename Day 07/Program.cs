759df0a78f3ce8c705f17ff56d33cc30f627efe8
Zachary Carey <zach.d.carey@gmail.com>
2022-12-09 01:06:13 -0500

    Day 07 pt 1

---
+++ Day 07/Program.cs
@@ -0,0 +1,46 @@
+﻿using AdventOfCode;
+using AdventOfCode.Parsing;
+using AdventOfCode.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_07 {
+    internal class Program : ProgramStructure<Directory> {
+
+        Program() : base(new Parser()
+            .Parse(new LineReader())
+            .ForEach(x => x.Split(" "))
+            .Create<Directory>()
+        ) { }
+
+        static void Main(string[] args) {
+            new Program().Run(args);
+        }
+
+        protected override object SolvePart1(Directory root) {
+            return Part1Recursive(root);
+        }
+
+        private long Part1Recursive(Directory search) {
+            long sum = 0;
+            if (search.Size <= 100000) {
+                sum += search.Size;
+            }
+
+            foreach(IFileSystem fileSystem in search.Children) {
+                if (fileSystem is Directory subdir) {
+                    sum += Part1Recursive(subdir);
+                }
+            }
+
+            return sum;
+        }
+
+
+        protected override object SolvePart2(Directory root) {
+            return null;
+        }
+
+    }
+}
