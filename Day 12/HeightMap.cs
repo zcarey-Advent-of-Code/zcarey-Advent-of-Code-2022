28c34db0829257a7dd540fdfad4997f37d5feec8
Zachary Carey <zach.d.carey@gmail.com>
2022-12-13 00:34:42 -0500

    Day 12 pt 2

--- Day 12/HeightMap.cs
+++ Day 12/HeightMap.cs
@@ -9,27 +9,28 @@ using System.Threading.Tasks;
 namespace Day_12 {
     public class HeightMap : IObjectParser<int[][]> {
 
-        private int[][] map;
+        public int[][] Map;
         public int Width { get; private set; }
         public int Height { get; private set; }
         public Point Start { get; private set; }
         public Point End { get; private set; }
-        public Rectangle Region => new Rectangle(0, 0, Width, Height);
+
+        public IEnumerable<Point> AllIndicies {
+            get {
+                for (int y = 0; y < Height; y++) {
+                    for (int x = 0; x < Width; x++) {
+                        yield return new Point(x, y);
+                    }
+                }
+            }
+        }
 
         public int[] this[int index] {
-            get => map[index];
-        }
-
-        public int GetDistance(Point A, Point B) {
-            return Math.Abs(A.X - B.X) + Math.Abs(A.Y - B.Y);
-        }
-
-        public IEnumerable<Point> ClimableNeighbors(Point p) {
-            return p.NeighborsWithinGrid(Region).Where(x => (map[x.Y][x.X] - map[p.Y][p.X]) <= 1);
+            get => Map[index];
         }
 
         public void Parse(int[][] input) {
-            this.map = input;
+            this.Map = input;
             this.Height = input.Length;
             this.Width = input[0].Length;
 
@@ -38,12 +39,12 @@ namespace Day_12 {
 
             for(int y = 0; y < Height; y++) {
                 for (int x = 0; x < Width; x++) {
-                    if (map[y][x] == 'S' - 'a') {
+                    if (Map[y][x] == 'S' - 'a') {
                         Start = new Point(x, y);
-                        map[y][x] = -1;
+                        Map[y][x] = -1;
                     } else if (input[y][x] == 'E' - 'a') {
                         End = new Point(x, y);
-                        map[y][x] = ('z' - 'a') + 1;
+                        Map[y][x] = ('z' - 'a') + 1;
                     }
                 }
             }
--- Day 12/Program.cs
+++ Day 12/Program.cs
@@ -21,63 +21,20 @@ namespace Day_12 {
             //new Program().Run(args, "Example.txt");
         }
 
-        protected override object SolvePart1(HeightMap input) {
-            return Dijkstra(input).Count - 1;
+        private static int DistanceFormula((Point p, int h) A, (Point p, int h) B) {
+            // Dont forget we are mapping in reverse
+            return ((A.h - B.h) <= 1) ? 1 : int.MaxValue;
         }
 
-        public static Stack<Point> Dijkstra(HeightMap map) {
-            int[][] dist = new int[map.Height][];
-            Point?[][] prev = new Point?[map.Height][];
-            PriorityQueue<Point, int> queue = new();
-
-            for(int y = 0; y < map.Height; y++) {
-                dist[y] = new int[map.Width];
-                prev[y] = new Point?[map.Width];
-                for (int x = 0; x < map.Width; x++) {
-                    Point v = new Point(x, y);
-                    dist[y][x] = int.MaxValue;
-                    prev[y][x] = null;
-                    if (v == map.Start) {
-                        dist[y][x] = 0; // Make sure it's processed first
-                    }
-                    queue.Enqueue(v, dist[y][x]);
-                }
-            }
-
-            while(queue.Count > 0) {
-                Point u = queue.Dequeue();
-
-                if (u == map.End) {
-                    // Woohoo! Return the path!
-                    Stack<Point> path = new();
-                    path.Push(u);
-                    while(u != map.Start) {
-                        u = (Point)prev[u.Y][u.X];
-                        path.Push(u);
-                    }
-                    return path;
-                }
-
-                foreach(Point v in map.ClimableNeighbors(u)) {
-                    int alt = dist[u.Y][u.X] + map.GetDistance(u, v);
-                    if (alt < dist[v.Y][v.X]) {
-                        dist[v.Y][v.X] = alt;
-                        prev[v.Y][v.X] = u;
-
-                        // Due to a limitation in the C# PriorityQueue, we cant change the priority of an already queued item.
-                        // However, due to the relatively small size of this problem we can afford calculating again if it pops
-                        // up in the queue again. Wastes a bit of memory, but considering this is Advent of Code and I have to
-                        // finish this up tonight I dont feel like writing my own PriorityQueue :)
-                        queue.Enqueue(v, alt); 
-                    }
-                }
-            }
-
-            throw new Exception("Unable to find path.");
+        protected override object SolvePart1(HeightMap input) {
+            //return Dijkstra(input).Count - 1;
+            Dijkstra<int> map = Dijkstra<int>.Generate(input.Map, input.End, DistanceFormula);
+            return map[input.Start].Distance;
         }
 
         protected override object SolvePart2(HeightMap input) {
-            return null;
+            Dijkstra<int> map = Dijkstra<int>.Generate(input.Map, input.End, DistanceFormula);
+            return input.AllIndicies.Where(p => input[p.Y][p.X] == 0).Select(p => map[p].Distance).Min();
         }
 
     }
